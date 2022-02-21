38f366af8eb2ef2565854c1910abc9fe8962ea24
slimecube98 <57504261+slimecube98@users.noreply.github.com>
2022-02-21 15:12:39 +0200

    added postinitscript

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
 #!/usr/bin/env node
+keytool -genkey -v -keystore debug.keystore -storepass android -alias androiddebugkey -keypass android -keyalg RSA -keysize 2048 -validity 10000 -dname "CN=Levon Terteryan,OU=Zenify,O=Zeroqode,L=Chisinau,ST=Moldova,C=MD"
